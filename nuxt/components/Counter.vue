c59c04686b724f0bfa0a26e1598d107b01759482
Jan Vlnas <git.noreply@jan.vlnas.cz>
2021-10-05 02:13:28 +0200

    Nuxt counter

---
+++ nuxt/components/Counter.vue
@@ -0,0 +1,27 @@
+<template>
+<div className="counter">
+  <button @click="subtract">-</button>
+  <pre>{{count}}</pre>
+  <button @click="add">+</button>
+</div>
+</template>
+
+<script>
+export default {
+  props: ['initialCount'],
+  data() {
+    return {
+      title: "Counter",
+      count: this.initialCount,
+    };
+  },
+  methods: {
+    subtract() {
+      this.count--;
+    },
+    add() {
+      this.count++;
+    }
+  }
+};
+</script>
--- nuxt/pages/index.vue
+++ nuxt/pages/index.vue
@@ -1,5 +1,8 @@
 <template>
+<div>
   <h1>Hello world!</h1>
+  <Counter initialCount="0" />
+</div>
 </template>
 
 <script>
